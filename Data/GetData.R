cbed9a2cdcd913e9b282408c6b45440dd8e01192
Guillaume Guénard <guillaume.guenard@gmail.com>
2022-08-03 15:58:23 -0400

    Examining suggested data sets

---
+++ Data/GetData.R
@@ -0,0 +1,42 @@
+##
+### Getting data to test ideas related to the estimation of the functional
+### beta diversity on the basis of quantitative traits.
+##
+### Would be even better to have a unifying framework to estimate/test for any
+### type of traits:
+##
+### Quantitative (numeric),
+### Semi-quantitative (ordered), and
+### Qqualitative (factor)
+##
+data("Tiahura", package = "adespatial")
+##
+Tiahura
+##
+data("aviurba", package = "ade4")
+##
+aviurba
+##
+data("plantTraits", package = "cluster")
+##
+plantTraits
+##
+data("antTraits", package = "mvabund")
+##
+antTraits
+##
+data("dummy", package = "FD")
+##
+data("tussock", package = "FD")
+##
+tussock
+##
+
+
+
+
+
+
+
+
+
